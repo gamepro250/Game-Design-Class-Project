a3d57982948e43468b246843b66f6e6241a1a770
Billy <wh1949@ship.edu>
2016-09-11 17:15:45 -0400

    Commit of all code added from Chapter 3.

--- desktop/src/com/billyharrisongdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/billyharrisongdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package com.billyharrisongdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.billyharrisongdx.game.CanyonBunnyMain;
 import com.billyharrisongdx.game.billyHarrisonGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new billyHarrisonGdxGame(), config);
+		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
