d27f7c9b1c3dcbd6fa59347d0574595827b88b75
Billy <wh1949@ship.edu>
2016-09-15 17:31:59 -0400

    Assignment 4: (Changed: Constants.java)

--- core/src/com/billyharrisongdx/game/util/Constants.java
+++ core/src/com/billyharrisongdx/game/util/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
 	public static final float VIEWPORT_HEIGHT = 5.0f ;
 
 	// Location of description file for texture atlas
-	public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack" ;
+	public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack.atlas" ;
 }
