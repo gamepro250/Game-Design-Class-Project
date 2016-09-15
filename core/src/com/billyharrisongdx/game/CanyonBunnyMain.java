77c72ef657d9770c0b2ae0db9c5739c6088a3f01
Billy <wh1949@ship.edu>
2016-09-15 08:50:33 -0400

    Assignment 4: (Changed: CanyonBunnyMain.java, WorldController.java, Constants.java, DesktopLauncher.java) (Added:Assets.java, Canyon Bunny Images)

--- core/src/com/billyharrisongdx/game/CanyonBunnyMain.java
+++ core/src/com/billyharrisongdx/game/CanyonBunnyMain.java
@@ -6,6 +6,8 @@ import com.billyharrisongdx.game.game.WorldRenderer ;
 import com.badlogic.gdx.Application ;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20 ;
+import com.badlogic.gdx.assets.AssetManager ;
+import com.billyharrisongdx.game.game.Assets ;
 
 public class CanyonBunnyMain implements ApplicationListener {
 		private static final String TAG =
@@ -20,6 +22,8 @@ public class CanyonBunnyMain implements ApplicationListener {
 	{
 		// Set Libgdx log level to DEBUG
 		Gdx.app.setLogLevel(Application.LOG_DEBUG) ;
+		// Load assets
+		Assets.instance.init(new AssetManager()) ;
 		// Initialize controller and renderer
 		worldController = new WorldController() ;
 		worldRenderer = new WorldRenderer(worldController) ;
@@ -63,5 +67,6 @@ public class CanyonBunnyMain implements ApplicationListener {
 	public void dispose()
 	{
 		worldRenderer.dispose() ;
+		Assets.instance.dispose() ;
 	}
 }
--- desktop/src/com/billyharrisongdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/billyharrisongdx/game/desktop/DesktopLauncher.java
@@ -4,9 +4,26 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.billyharrisongdx.game.CanyonBunnyMain;
 import com.billyharrisongdx.game.billyHarrisonGdxGame;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker ;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings ;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
+		boolean rebuildAtlas = false ;
+		boolean drawDebugOutline = true ;
+
+		if(rebuildAtlas)
+		{
+			Settings settings = new Settings() ; // A group of settings defined for TexturePacker
+			settings.maxWidth = 1024 ; // Width of atlas
+			settings.maxHeight = 1024 ; // Height of atlas
+			settings.duplicatePadding = false ;
+			settings.debug = drawDebugOutline ; // draws square outline around each image in atlas
+			TexturePacker.process(settings, "assets-raw/images","../core/assets/images", "canyonbunny.pack") ; // packs images from source into destination
+		}
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
